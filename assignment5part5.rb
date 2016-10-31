61480bfecc0afa5b02244f0eee84544fd25ca65b
vmswift <scompy@gmail.com>
2016-10-31 15:38:02 -0500

    Update assignment5part5.rb

--- assignment5part5.rb
+++ assignment5part5.rb
@@ -1,18 +1,24 @@
+#!/usr/bin/env ruby
+#Assignmnet #5 Part 5
+#CS417 OOP
+#By John Knowles
+#Due November 1, 2016
+
 class List
 	def initialize()
 	@lists = Hash.new
 	@size = 0
 	end
 	
-	def insert(val)
-		@lists.store(@size+1,val)
+	def insert(value)
+		@lists.store(@size+1,value)
 		@size+=1
 	end
 	
-	def prints()
-		print @lists
-		puts
-	end
+#	def prints()
+#		print @lists
+#		puts
+#	end
 	
 	def traverse()
 		counter=1
@@ -20,18 +26,19 @@ class List
 			print @lists[counter].to_s + " "
 			counter+=1
 		end while counter <= @lists.length+1
+		puts
 	end
 	
-	def delete(val)
+	def delete(value)
 		counter=1
-		myVal = @lists[counter]
+		myValue = @lists[counter]
 		begin
-			if(myVal == val) then
+			if(myValue == value) then
 			@lists.delete(counter)
 			@lists.rehash
 			end
 		counter+=1
-		myVal = @lists[counter]
+		myValue = @lists[counter]
 		end while counter <= @lists.length+1
 	end
 end
@@ -41,12 +48,9 @@ myList = List.new()
 myList.insert("Bacon")
 myList.insert("Sasage")
 myList.insert("Ham")
-myList.prints()
 myList.traverse()
 myList.delete("Bacon")
-myList.prints()
 myList.insert("Pork")
 myList.insert("Sausage")
 myList.insert("Pig")
-myList.prints()
 myList.traverse()
