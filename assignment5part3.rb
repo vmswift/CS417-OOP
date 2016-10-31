4febe81a700dcaf06e365ad9e5bd5ede3bcaf520
vmswift <scompy@gmail.com>
2016-10-31 15:36:26 -0500

    Update assignment5part3.rb

--- assignment5part3.rb
+++ assignment5part3.rb
@@ -1,6 +1,13 @@
+#!/usr/bin/env ruby
+#Assignmnet #5 Part 3
+#CS417 OOP
+#By John Knowles
+#Due November 1, 2016
+
 def secondSmallest(list)
 	if (list.size > 2) then
-		newList = list.sort
+		newList = list.sort{|a,b| a.to_i<=>b.to_i}
+		#sort list least to greatest
 		secondSmall = newList[1]
 		return list.index(secondSmall)
 	else
